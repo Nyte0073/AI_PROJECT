a84f265b0400953a99d2365475b4de65a5d7d1fe
Nyte0073 <808563@pdsb.net>
2024-12-22 19:34:58 -0500

    Second Commit.

---
+++ src/GUI/Main_Window.java
@@ -0,0 +1,14 @@
+package GUI;
+
+import javax.swing.*;
+public class Main_Window {
+
+    JFrame frame;
+
+    public void startGUI() {
+        frame = new JFrame("AI Window");
+    }
+    public static void main(String[] args) {
+
+    }
+}
